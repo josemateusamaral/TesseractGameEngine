7ff8190327eddc8615885c67817f58c3de2c970a
José Mateus Amaral de Miranda <josemateusamaral@gmail.com>
2026-05-14 08:02:06 -0300

    - lighting test.

--- teste.cpp
+++ teste.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *args[])
 	}
 
 	// load model
-	Model* model = new Model("samples/model_loading/cubo.glb");
+	Model* model = new Model("samples/model_loading/esfere.glb");
 	model->setPos(0, 0, 14);
 	model->setScale(3);
 	model->setBackfaceCulling(true);
@@ -29,14 +29,14 @@ int main(int argc, char *args[])
 	tesseract.scene->addModel(model);
 
 	// create ambient light
-	DirectionalLight* ambientLight = new DirectionalLight( 1, 1, 1, 1.0, 1.0, 0.0);
+	DirectionalLight* ambientLight = new DirectionalLight( 1, 1, 1, -1.0, 0.0, 0.0);
 	model->setLight(ambientLight);
-	AmbientLight* directionalLight = new AmbientLight( 0.1, 0.1, 0.1);
+	AmbientLight* directionalLight = new AmbientLight( 0.5,0.5,0.5);
 	model->setLight(directionalLight);
 
 	tesseract.run([&]() {
 
-        model->rotate(1, 1, 1);
+        model->rotate(1, 0, 0);
         
     });
 	
